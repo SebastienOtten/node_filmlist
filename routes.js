7f643284509fd0cbcc46c2cafbc2acd2d4990809
SebastienOtten <78730474+SebastienOtten@users.noreply.github.com>
2021-05-10 17:44:10 +0200

    Add files via upload

--- routes.js
+++ routes.js
@@ -24,6 +24,12 @@ router.get('/film/delete/:i',filmController.filmDelete);
 //consulter une fiche
 router.get('/film/consult/:i',filmController.filmConsult);
 
+//voir la liste des categories
+router.get('/film/categorie', filmController.filmCategorie);
+
+//voir les films d'une catégorie
+router.get('/film/categorie/consult/:i', filmController.categorieConsult);
+
 /************API*************/
 
 //Voir la liste des films
@@ -32,7 +38,7 @@ router.get('/api/film', filmControllerApi.filmList);
 //Ajouter un film
 router.post('/api/film',filmControllerApi.filmNew);
 
-//Formulaire pour la modification d'une fiche de film
+//Consulter une fiche
 router.get('/api/film/:i',filmControllerApi.film);
 
 //Modifier le film dans la Database
@@ -40,5 +46,11 @@ router.put('/api/film/:i',filmControllerApi.filmUpdate);
 
 //Effacer une fiche de film
 router.delete('/api/film/:i',filmControllerApi.filmDelete);
+
+//voir la liste des categories
+router.get('/api/categorie',filmControllerApi.filmCategorie);
+
+//voir les films d'une catégorie
+router.get('/api/film/categorie/consult/:i',filmControllerApi.categorieConsult);
 
 module.exports = router;
